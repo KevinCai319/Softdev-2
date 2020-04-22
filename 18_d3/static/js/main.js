60bb6b49a80fee0128f8b51970b2a168bffa9551
kevin16777216 <k1029384756c@gmail.com>
2020-04-22 09:52:13 -0400

    done?

--- 18_d3/static/js/main.js
+++ 18_d3/static/js/main.js
@@ -1,3 +1,5 @@
+var schoolData = [];
+var schoolID = 1;
 const d3Load = (type,fp,key) => {
   type(fp)
     .then((data) => {
@@ -18,13 +20,89 @@ const loadData = (key, fp, ext) => {
     return rawData;
 };
 
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+var svg = d3.select("#plot")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+// Initialise a X axis:
+var x = d3.scaleLinear().range([0,width]);
+var xAxis = d3.axisBottom().scale(x);
+svg.append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .attr("class","myXaxis");
+
+// Initialize an Y axis
+var y = d3.scaleLinear().range([height, 0]);
+var yAxis = d3.axisLeft().scale(y);
+svg.append("g")
+   .attr("class","myYaxis");
+
 loadPlot = ()=>{
   console.log('yo');
-  console.log(loadData('demo','/static/csv/2019-2020.csv',d3.csv));
+  schoolData = [];
+  let dir = '';
+  for(i = 2015;i < 2020; i++){
+    dir = '/static/csv/'+i.toString()+'-'+(i+1).toString()+'.csv';
+    console.log(dir);
+    schoolData.push(loadData('year'+i.toString(),dir,d3.csv));
+  }
 };
 
 showPlot = ()=>{
-  console.log('hi');
+  let tmp = []
+  let max = 5;
+  let num = 0;
+  let name = schoolData[0][schoolID]["Feeder School Name"];
+  for(let i = 0; i < schoolData.length; i++){
+      num = schoolData[i][schoolID]["Count of Offers"];
+      console.log(num);
+      console.log(schoolData[i][schoolID]);
+      if(num === "0-5"){
+        num = 0;
+      }else{
+        num = parseInt(num);
+      }
+      tmp.push({year:(i+2015), accept:num});
+      max = Math.max(max, num);
+  }
+  if(max == 5){
+    schoolID += 1;
+    showPlot();
+    return;
+  }
+  console.log(name);
+  x.domain([2015,2015+schoolData.length]);
+  svg.selectAll(".myXaxis").transition()
+    .duration(3000)
+    .call(xAxis);
+  // console.log(tmp);
+  // create the Y axis
+  y.domain([0, max+4]);
+  svg.selectAll(".myYaxis")
+    .transition()
+    .duration(3000)
+    .call(yAxis);
+    var u = svg.selectAll(".lineTest")
+    .data([tmp], function(d){ return d.accept });
+  // Updata the line
+  u
+    .enter()
+    .append("path")
+    .attr("class","lineTest")
+    .merge(u)
+    .transition()
+    .duration(3000)
+    .attr("d", d3.line().x(function(d) { return x(d.year); }).y(function(d) { return y(d.accept); }))
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 2.5)
+  schoolID += 1;
 };
 
 
